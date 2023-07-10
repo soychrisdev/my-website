1c56f8050c123227098baabef5a880b7050838a9
Christian Mesina <121773709+soychrisdev@users.noreply.github.com>
2023-07-10 15:41:44 +0000

    useEffect update button

--- components/main/test/modal.tsx
+++ components/main/test/modal.tsx
@@ -2,11 +2,15 @@
 
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function ModalTest() {
     const [open, setOpen] = useState(false);
-    console.log(open)
+
+    useEffect( ()=> {
+console.log(`EL MODAL SE ENCUENTRA: ${open}`)
+    }, [open])
+  
 
     return (
         <>
